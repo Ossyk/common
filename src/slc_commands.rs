5b20f3fe8e4eb97d887e38c5720a2b83d86ad14f
SecondarySkyler <skylerhunter6@gmail.com>
2025-01-23 18:47:49 +0100

    removed drone command/event

--- src/slc_commands.rs
+++ src/slc_commands.rs
@@ -1,39 +1,24 @@
-use wg_2024::{network::NodeId, packet::Packet};
 use crossbeam_channel::Sender;
+use wg_2024::{network::NodeId, packet::Packet};
 // todo: receive messages, web page, drone events
 // received nacks from drones should be redirected to the source (client/server)
 // Commands sent by the Simulation Controller to a Client
 pub enum ClientCommand {
     AddSender(NodeId, Sender<Packet>),
-    SendChatText(String)
+    SendChatText(String),
 }
 
 // Commands sent by the Simulation Controller to a Server
 pub enum ServerCommand {
-    AddSender(NodeId, Sender<Packet>)
-}
-
-// Commands sent by the Simulation Controller to a Drone
-pub enum DroneCommand {
-    Crash,
-    RemoveSender(NodeId),
     AddSender(NodeId, Sender<Packet>),
-    SetPacketDropRate(f32)
 }
 
 // Command sent by a Client to the Simulation Controller
 pub enum ClientEvent {
-    PacketSent(Packet)
+    PacketSent(Packet),
 }
 
 // Command sent by a Server to the Simulation Controller
 pub enum ServerEvent {
-    PacketSent(Packet)
-}
-
-// Command sent by a Drone to the Simulation Controller
-pub enum DroneEvent {
     PacketSent(Packet),
-    PacketDropped(Packet),
-    ControllerShortcut(Packet)
 }
