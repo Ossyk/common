2502ca92afa149e9852ac7cb171ac96f519f20be
SecondarySkyler <skylerhunter6@gmail.com>
2025-01-24 16:26:20 +0100

    derived Debug and Clone traits

--- src/slc_commands.rs
+++ src/slc_commands.rs
@@ -3,6 +3,7 @@ use crossbeam_channel::Sender;
 use wg_2024::{network::NodeId, packet::Packet};
 
 // Commands sent by the Simulation Controller to a Client
+#[derive(Debug, Clone)]
 pub enum ClientCommand {
     AddSender(NodeId, Sender<Packet>),
     SendChatText(String, NodeId, NodeId), // text, client_id, chat_server_id
@@ -13,11 +14,13 @@ pub enum ClientCommand {
 }
 
 // Commands sent by the Simulation Controller to a Server
+#[derive(Debug, Clone)]
 pub enum ServerCommand {
     AddSender(NodeId, Sender<Packet>),
 }
 
 // Command sent by a Client to the Simulation Controller
+#[derive(Debug, Clone)]
 pub enum ClientEvent {
     PacketSent(Packet),
     Shortcut(Packet),
@@ -30,10 +33,12 @@ pub enum ClientEvent {
 }
 
 // Command sent by a Server to the Simulation Controller
+#[derive(Debug, Clone)]
 pub enum ServerEvent {
     PacketSent(Packet),
 }
 
+#[derive(Debug, Clone)]
 pub enum ServerType {
     ChatServer,
     FileServer,
