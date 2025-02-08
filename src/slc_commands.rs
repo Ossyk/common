8ea1de22be4b1e0b3f98c7533dec17e2a10013d8
SecondarySkyler <skylerhunter6@gmail.com>
2025-02-08 15:32:40 +0100

    temp comments

--- src/slc_commands.rs
+++ src/slc_commands.rs
@@ -99,9 +99,9 @@ pub enum ChatClientCommand {
     /// Ask a client to discover the server types in the network
     AskServersTypes,
     /// Ask a client to connect to the server identified by the given ID
-    ConnectToChatServer(NodeId), // chat_server_id
+    //ConnectToChatServer(NodeId), // chat_server_id
     /// Ask a client to send a message (first parameter) to anothe client (second parameter) through a chat server (third parameter)
-    SendChatText(String, NodeId, NodeId), // text, client_id, chat_server_id
+    //SendChatText(String, NodeId, NodeId), // text, client_id, chat_server_id
     /// Scl shortcuts a packet to the client
     Shortcut(Packet),
     /// Send a text message for processing by the client
@@ -120,9 +120,9 @@ pub enum ChatClientEvent {
     /// communicate to scl the servers type of the servers in the network
     ServersTypes(HashMap<NodeId, ServerType>), // server_id, server_type
     /// communicate to scl all the clients connected to a chat server
-    ClientsConnectedToChatServer(NodeId, Vec<NodeId>),
+    //ClientsConnectedToChatServer(NodeId, Vec<NodeId>),
     /// communicate to scl that a new message has arrived from the node with the given ID
-    NewMessageFrom(NodeId), // client_id, maybe add also chat_server_id
+    //NewMessageFrom(NodeId), // client_id, maybe add also chat_server_id
     /// inform that client received an unsupported request
     UnsupportedRequest,
     /// Receive text to print out
